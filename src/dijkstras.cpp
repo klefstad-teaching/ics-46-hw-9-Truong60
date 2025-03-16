63ac7d795440b42fb4c21dcace92acc1d867cef0
Truong Tran <truontt1@uci.edu>
2025-03-16 08:52:55 +0000

    first param in extract not ness

--- src/dijkstras.cpp
+++ src/dijkstras.cpp
@@ -31,12 +31,9 @@ vector<int> dijkstra_shortest_path(const Graph& G, int source, vector<int>& prev
     return distances;
 }
 
-vector<int> extract_shortest_path(const vector<int>& distances, const vector<int>& previous, int destination)
+vector<int> extract_shortest_path(const vector<int>&, const vector<int>& previous, int destination)
 {   
     vector<int> store;
-    if (distances[destination] == -1){
-        return distances;
-    }
     for (int curr = destination; curr != -1; curr = previous[curr]){
         store.push_back(curr);
     }
