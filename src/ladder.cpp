35a8ec912c5ca07ae9b0dde6a290716e00502835
Truong Tran <truontt1@uci.edu>
2025-03-16 08:58:22 +0000

    implemented error func

---
+++ src/ladder.cpp
@@ -0,0 +1,9 @@
+#include "ladder.h"
+
+using namespace std;
+
+void error(string word1, string word2, string msg)
+{
+    cout << word1 << " " << msg << endl;
+    cout << word2 << " " << msg << endl;
+}
