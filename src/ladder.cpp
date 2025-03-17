618d9b085d8e9923b51529e588fce1167d55f108
Truong Tran <truontt1@uci.edu>
2025-03-17 02:29:37 +0000

    implemented verify and load

--- src/ladder.cpp
+++ src/ladder.cpp
@@ -11,7 +11,7 @@ void error(string word1, string word2, string msg)
 bool edit_distance_within(const std::string& str1, const std::string& str2, int d)
 {   
     int d_counter = 0; 
-    int str1INDEX = 0, str2INDEX = 0;
+    size_t str1INDEX = 0, str2INDEX = 0;
     if (str1 == str2){
         return true;
     }
@@ -75,11 +75,31 @@ vector<string> generate_word_ladder(const string& begin_word, const string& end_
     }
     return empty;
 }
-void load_words(set<string> & word_list, const string& file_name){}
-void print_word_ladder(const vector<string>& ladder)
+void load_words(set<string> & word_list, const string& file_name)
 {
-    for (int i = 0; i < ladder.size(); i++){
-        cout << ladder[i] << " ";
+    ifstream file_to_copy(file_name);
+    string word;
+    while (file_to_copy >> word){
+        word_list.insert(word);
     }
 }
-void verify_word_ladder(){}
+
+void print_word_ladder(const vector<string>& ladder)
+{
+    for (size_t i = 0; i < ladder.size(); i++){
+        cout << "Word ladder found:" << ladder[i] << " ";
+    }
+}
+
+#define my_assert(e) {cout << #e << ((e) ? " passed": " failed") << endl;}
+void verify_word_ladder()
+{
+    set<string> word_list;
+    load_words(word_list, "words.txt");
+    my_assert(generate_word_ladder("cat", "dog", word_list).size() == 4);
+    my_assert(generate_word_ladder("marty", "curls", word_list).size() == 6);
+    my_assert(generate_word_ladder("code", "data", word_list).size() == 6);
+    my_assert(generate_word_ladder("work", "play", word_list).size() == 6);
+    my_assert(generate_word_ladder("sleep", "awake", word_list).size() == 8);
+    my_assert(generate_word_ladder("car", "cheat", word_list).size() == 4);
+}
