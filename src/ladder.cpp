a4d3cc67b752a842afb0d1f492bdd8abaf18fcee
Truong Tran <truontt1@uci.edu>
2025-03-17 00:26:47 +0000

    incrementing

--- src/ladder.cpp
+++ src/ladder.cpp
@@ -12,15 +12,22 @@ bool edit_distance_within(const std::string& str1, const std::string& str2, int 
 {   
     int d_counter = 0; 
     for (int str1INDEX = 0, str2INDEX = 0; str1INDEX < str1.length() && str2INDEX < str2.length(); str1INDEX++, str2INDEX++){
-        if (str1[str1INDEX] != str2[str2INDEX]){
+        if (str1[str1INDEX] == str2[str2INDEX]){
+            str1INDEX++;
+            str2INDEX++;
+        }
+        else if (str1[str1INDEX] != str2[str2INDEX]){
             d_counter++;
             if (d_counter > d){
                 return false;
             }
             if (str1.length() > str2.length()){
-                str2INDEX--;
+                str2INDEX++;
             } else if (str2.length() > str1.length()){
-                str1INDEX--;
+                str1INDEX++;
+            } else{
+                str1INDEX++;
+                str2INDEX++;
             }
         }
     }
